c2930162cf62e4e66395502d4b37480ff13bf8c9
Reformist <Reformist@Reformist-PC>
2014-12-27 13:03:14 +0530

    fist mt second

--- repostryexampe/src/MainClass.java
+++ repostryexampe/src/MainClass.java
@@ -6,6 +6,9 @@ public class MainClass {
 	 */
 	public static void main(String[] args) {
 		System.out.println("IN main");
+		Chotaclass cc=new Chotaclass();
+		cc.setData();
+		System.out.println("IN GIT");
 
 	}
 
